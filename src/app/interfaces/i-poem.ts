ce915f86fbc649fe635ff7e8a2ae871ec3a349b8
Ash <ash.norseman@gmail.com>
2019-05-24 16:36:10 +0800

    《詩經》全文

--- src/app/interfaces/i-poem.ts
+++ src/app/interfaces/i-poem.ts
@@ -4,6 +4,7 @@
 
 export interface IPoem {
   category: string;
+  subCategory: string;
   title: string;
   content: string[];
 }
--- src/app/pages/shi-jing/shi-jing.component.ts
+++ src/app/pages/shi-jing/shi-jing.component.ts
@@ -5,7 +5,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { shijing } from '../../data/shijing';
-import { sounds } from '../../data/sounds';
+// import { sounds } from '../../data/sounds';
 import { IPoem } from '../../interfaces/i-poem';
 
 @Component({
@@ -22,18 +22,20 @@ export class ShiJingComponent {
       return {
         ...poem,
         content: poem.content.map(paragraph => {
-          return paragraph
-            .split('')
-            .map(char => {
-              const sound = sounds.find(item => item.char === char);
-
-              if (sound) {
-                return `<span class="char"><ruby>${char}<rt>${sound.sound}</rt></ruby></span>`;
-              } else {
-                return char;
-              }
-            })
-            .join('');
+          return (
+            paragraph
+              .split('')
+              // .map(char => {
+              //   const sound = sounds.find(item => item.char === char);
+              //
+              //   if (sound) {
+              //     return `<span class="char"><ruby>${char}<rt>${sound.sound}</rt></ruby></span>`;
+              //   } else {
+              //     return char;
+              //   }
+              // })
+              .join('')
+          );
         })
       };
     });
